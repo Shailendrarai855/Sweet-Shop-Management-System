c09dee9fda8629c9b891eb480b528580c227896f
shailendrarai855 <shailendrarai855@gmail.com>
2025-11-14 21:38:27 +0530

    added server file generated via spring initializer

---
+++ sweet-shop-server/src/main/java/com/sweet_shop_server/sweet_shop_server/SweetShopServerApplication.java
@@ -0,0 +1,13 @@
+package com.sweet_shop_server.sweet_shop_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetShopServerApplication.class, args);
+	}
+
+}
---
+++ sweet-shop-server/src/test/java/com/sweet_shop_server/sweet_shop_server/SweetShopServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweet_shop_server.sweet_shop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
